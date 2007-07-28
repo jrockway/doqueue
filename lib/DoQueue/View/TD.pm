9cea69a1ae41fbfd840fd7722463762e9f729fc6
Jonathan Rockway <jon@jrock.us>
2007-07-28 12:09:31 -0500

    add TD view

---
+++ lib/DoQueue/View/TD.pm
@@ -0,0 +1,49 @@
+package DoQueue::View::TD;
+use strict;
+use warnings;
+
+use base 'Catalyst::View::Template::Declare';
+use Template::Declare::Tags;
+
+# Test Template, replace with your own
+# template test => sub {
+#     html {
+#         head {
+#             title {"This is a test" }
+#         };
+#         body {
+#             h1 { "This is a test" };
+#             p { "Welcome to Template::Declare." }
+#         }
+#     }
+# };
+#
+# You can also put templates in separate modules, each under the
+# DoQueue::View::TD::* prefix.
+
+  
+=head1 NAME
+
+DoQueue::View::TD - Template::Declare View for DoQueue
+
+=head1 DESCRIPTION
+
+Template::Declare View for DoQueue. 
+
+=head1 AUTHOR
+
+Jonathan T. Rockway,,,
+
+=head1 SEE ALSO
+
+L<DoQueue>
+
+L<Catalyst::View::Template::Declare>
+
+L<Template::Declare::Tags>
+
+L<Template::Declare>
+
+=cut
+
+1;
